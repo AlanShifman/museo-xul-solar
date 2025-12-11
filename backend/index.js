6773f75872aedcf46ba8fe7f8a17f8128dabf4db
AlanShifman <50155769@est.ort.edu.ar>
2025-12-11 12:09:49 -0300

    hice 1, 2 y 3

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,48 @@ startServer();
 
 subscribeGETEvent ("colección", coleccions)
 function coleccions () {
-    let contenido = fs.readFileSync("../data/obras.json","utf-8");
+    let contenido = fs.readFileSync("../data/idcoleccion.json","utf-8");
     return JSON.parse (contenido)
 }
+
+subscribePOSTEvent  ("modificarColección", numero)
+
+function numero (data) {
+
+    if (data.enColección == true) {
+        let contenido = JSON.parse(fs.readFileSync("../data/idcoleccion.json","utf-8"));
+        
+        contenido.push(data.id);
+
+        let idcoleccionjson = JSON.stringify(contenido, null, 2);
+        fs.writeFileSync("../data/idcoleccion.json", idcoleccionjson);
+
+        return true;
+    }
+    else {
+        let contenido = JSON.parse(fs.readFileSync("../data/idcoleccion.json","utf-8"));
+
+        let resultado = contenido.filter(id => id !== data.id);
+    
+            let idcoleccionjson = JSON.stringify(resultado, null, 2);
+            fs.writeFileSync("../data/idcoleccion.json", idcoleccionjson);
+    
+
+
+        return false;
+
+    }
+}
+
+subscribeGETEvent("obrasColección", devolverColeccion);
+
+function devolverColeccion() {
+    let ids = JSON.parse(fs.readFileSync("../data/idcoleccion.json", "utf-8"));
+    let obras = JSON.parse(fs.readFileSync("../data/obras.json", "utf-8"));
+
+  
+
+    return ;
+}
+
 
