13c7a7580e8a6cd024912aad2a5176bf5447bc9a
AlanShifman <50155769@est.ort.edu.ar>
2025-12-16 22:07:54 -0300

    final con anotacions

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,8 @@ subscribeGETEvent ("obras", hola) // cuando el front end pida el evento obras ej
 
 function hola() {  
     let contenido = fs.readFileSync("../data/obras.json","utf-8");
-    return JSON.parse(contenido); // lee el archivo con las obras en texto y lo devuelve como un array. data obras json es el archivo que leemos y utf-8 es para leerlo en texto y no en binario.
-} 
+    return JSON.parse(contenido);// data obras json es el archivo que leemos y utf-8 es para leerlo en texto y no en binario. let creaa variables.
+} // resumen: el front end le esta pidiendo al back end que cuando llame el evento obras, se ejecute una funcion que le permite al backend devolver el archivo de las obras pasado de texto a array.
 
 startServer();
 
@@ -14,25 +14,26 @@ subscribeGETEvent ("colección", coleccions) // cuando el frontend pida el event
 function coleccions () {
     let contenido = fs.readFileSync("../data/idcoleccion.json","utf-8"); 
     return JSON.parse (contenido)// lee el archivo que contiene los id y lo devuelve como un array al frontend.
-}
+}// cuando el frontend pide el evento collecion se ejecuta la funcion collections, en la cual let crea una variable llmada contenido que lee el archivo de los ides en texto, y luego json.parse convierte el archivo a array para devolverselo al frontend.
 
 subscribePOSTEvent  ("modificarColección", numero) // cuando el back recibe el evento modificar coleccion se ejecuta la funcion numero.
-function numero (data) {
+function numero (data) { // data es el objeto que manda el frontend. tiene la informacion de los ids y si es true or false ( agregarlos o no) // booleano es un dato que tiene 2 valores, true y false.
 
-    if (data.enColección == true) {
-        let contenido = JSON.parse(fs.readFileSync("../data/idcoleccion.json","utf-8"));
+    if (data.enColección == true) { // en coleccion que indica true si la obra debe agregarse o false si la obra no debe agregarse.
+        let contenido = JSON.parse(fs.readFileSync("../data/idcoleccion.json","utf-8")); // leo el archivo de los ids en texto, lo convierto en array y lo guardo en la variable contenido.
         
-        contenido.push(data.id);
+        contenido.push(data.id); // guarda el id recibido desde el front end en la variable contenido, que contiene el array del archivo con todos los ids.(push guarda) data.id es el id que llega desde el frontend.
 
-        let idcoleccionjson = JSON.stringify(contenido, null, 2);
-        fs.writeFileSync("../data/idcoleccion.json", idcoleccionjson);
+        let idcoleccionjson = JSON.stringify(contenido, null, 2); // vuelvo a convertir el array en json (texto) null 2 es para hacerlo con sangria.
+        fs.writeFileSync("../data/idcoleccion.json", idcoleccionjson); // guardo los ids seleccionados (idcolleccionjson) en el archivo de los ids
 
-        return true;
-    }
+        return true; // devuelve true al frontend
+    } // cuando el backend recibe el evento modificarcolleccion, se ejecuta la funcion numero, que si data es true, lee el archivo de los ids en teeexto, y lo convierte de json a array y lo guarda en la variable contenido. luego la funcion push guarda en ls variable contenido que contiene el array del archivo de los ids el id que selecciono el frontend. luego se vuelve a convertir el archivo de los ids de array a json y se fuarda en la variable contenido, y luego esta variable se guarda en el archivo de los ids. 
     else {
-        let contenido = JSON.parse(fs.readFileSync("../data/idcoleccion.json","utf-8"));
+        let contenido = JSON.parse(fs.readFileSync("../data/idcoleccion.json","utf-8")); // lee el archivo de los ids lo convierte a array y lo guarda en la variable contenido.
 
-        let resultado = contenido.filter(id => id !== data.id);
+
+        
     
             let idcoleccionjson = JSON.stringify(resultado, null, 2);
             fs.writeFileSync("../data/idcoleccion.json", idcoleccionjson);
